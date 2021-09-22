dbfd659004858870198c965aa4f78c255eddc67c
Nandhika Prayoga <nandhika.dev@gmail.com>
2021-09-23 04:47:10 +0700

    Prettify calendar input

--- scenes/Dashboard/components/Home/components/Field/index.js
+++ scenes/Dashboard/components/Home/components/Field/index.js
@@ -1,54 +1,71 @@
-import { useRef, useState, useCallback } from "react";
+import { useState } from "react";
 import Datetime from "react-datetime";
+import NumberFormat from "react-number-format";
+import moment from "moment";
+import "moment/locale/id";
 
-import { Box, Gap, Click, Cloud } from "@components/index";
+import { Box, Click } from "@components/index";
 import { DownArrowIcon } from "@icons/index";
 import { COLORS } from "@constants/index";
 import "react-datetime/css/react-datetime.css";
 
+import { DATE_FORMAT } from "./contants";
 import * as Styled from "./styled";
+
+moment.updateLocale("id", {
+  weekdaysMin: ["M", "S", "S", "R", "K", "J", "S"],
+});
 
 export default function Field({
   value = "",
   type = "text",
   placeholder = "",
   onChange = () => {},
-  width,
 }) {
-  const inputRef = useRef();
   const [dateIsOpened, setDateIsOpened] = useState(false);
-
-  const handleClick = () => {
-    if (type === "date") {
-      setDateIsOpened(!dateIsOpened);
-    }
-
-    if (inputRef) {
-      inputRef.current.focus();
-    }
-  };
 
   if (type === "date") {
     return (
       <Click radius="0.25rem" background={COLORS.WHITE} width="100%">
         <Styled.CustomDatetime>
           <Datetime
-            dateFormat="DD MMM YYYY"
+            inputProps={{
+              placeholder,
+              disabled: false,
+            }}
+            renderInput={(props) => (
+              <NumberFormat
+                {...props}
+                format={DATE_FORMAT.replace(/[^\/]/g, "#")}
+                mask={DATE_FORMAT.replace(/\//g, "").split("")}
+              />
+            )}
+            dateFormat="DD/MM/YYYY"
             timeFormat={false}
             onOpen={() => setDateIsOpened(true)}
             onClose={() => setDateIsOpened(false)}
+            onChange={(moment) => onChange(moment.toDate())}
             closeOnClickOutside={() => setDateIsOpened(false)}
           />
         </Styled.CustomDatetime>
+        <Box grow={1} />
         <Box
           transform={`rotateZ(${dateIsOpened ? 0 : -90}deg)`}
-          margin="0 1rem"
+          margin="0 1rem 0 0"
         >
-          <DownArrowIcon side={16} />
+          <DownArrowIcon height={16} width={16} />
         </Box>
       </Click>
     );
   }
 
-  return <Styled.Input placeholder={placeholder} value={value} />;
+  return (
+    <Styled.Input
+      placeholder={placeholder}
+      value={value}
+      onChange={(element) => {
+        onChange(element.target.value);
+      }}
+    />
+  );
 }
--- scenes/Dashboard/components/Home/components/Field/styled.js
+++ scenes/Dashboard/components/Home/components/Field/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 import { Box } from "@components/index";
 import { COLORS, SIZES } from "@constants/index";
-import { isNone } from "@helpers/utilities";
 
 export const Input = styled.input`
   color: ${COLORS.WARM_BLACK};
@@ -15,11 +14,56 @@ export const Input = styled.input`
 
 export const CustomDatetime = styled(Box)`
   & input {
+    placeholder ${(props) => props.placeholder}
     color: ${COLORS.WARM_BLACK};
     font-size: ${SIZES.REGULAR};
     width: 100%;
     padding: 1rem;
     border-radius: 0.25rem 0 0 0.25rem;
     background: transparent;
+  }
+
+  .rdtPicker {
+    width: 280px;
+    font-family: Lato !important;
+    font-size: 0.75rem;
+    padding: 1rem;
+    margin: 1rem 0 0 0;
+    border-radius: 0.25rem;
+  }
+
+  thead {
+    tr:nth-child(1) {
+      height: 4rem;
+      font-size: 1.25rem;
+
+      th {
+        vertical-align: middle;
+      }
+    }
+
+    tr:nth-child(2) {
+      font-size: 1rem;
+      color: ${COLORS.GRAY}
+    }
+  }
+
+  .rdtDay,
+  .rdtMonth,
+  .rdtYear,
+  .rdtSwitch {
+    &:hover {
+      border-radius: 1000px;
+    }
+  }
+
+  .rdtActive {
+    background: ${COLORS.RED};
+    border-radius: 1000px;
+  }
+
+  td.rdtDay {
+    height: 2.5rem;
+    width: 2.5rem;
   }
 `;
